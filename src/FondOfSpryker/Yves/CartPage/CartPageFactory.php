f85e0adf81d4993dd6808055633dbf7b1d7a160c
Julian Zimmermann <julian.zimmermann@fondof.de>
2020-06-02 20:35:40 +0200

    - removed hotfix since spryker fixed the bug

--- src/FondOfSpryker/Yves/CartPage/CartPageFactory.php
+++ src/FondOfSpryker/Yves/CartPage/CartPageFactory.php
@@ -2,7 +2,6 @@
 
 namespace FondOfSpryker\Yves\CartPage;
 
-use FondOfSpryker\Yves\CartPage\Form\FormFactory;
 use FondOfSpryker\Yves\CartPage\Model\CartItemReader;
 use SprykerShop\Yves\CartPage\CartPageFactory as SprykerShopCartPageFactory;
 
@@ -38,13 +37,5 @@ class CartPageFactory extends SprykerShopCartPageFactory
     public function getShouldRedirectToCartAfterAddToCart(): bool
     {
         return $this->getConfig()->getShouldRedirectToCartAfterAddToCart();
-    }
-
-    /**
-     * @return \FondOfSpryker\Yves\CartPage\Form\FormFactory
-     */
-    public function createFondOfCartPageFormFactory(): FormFactory
-    {
-        return new FormFactory();
     }
 }
