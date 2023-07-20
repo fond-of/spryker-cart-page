7b56109f2b16d4ffb835148058023b84d5380b39
Pascal Fischert <p-fischer@gmx.ne>
2023-07-20 14:42:49 +0200

    revert factory changes, replace deprecated store instance initialization

--- src/FondOfSpryker/Yves/CartPage/CartPageFactory.php
+++ src/FondOfSpryker/Yves/CartPage/CartPageFactory.php
@@ -2,8 +2,10 @@
 
 namespace FondOfSpryker\Yves\CartPage;
 
+use FondOfSpryker\Yves\CartPage\Dependency\Client\CartPageToLocaleClientInterface;
 use FondOfSpryker\Yves\CartPage\Dependency\Client\CartPageToProductImageStorageClientInterface;
 use FondOfSpryker\Yves\CartPage\Model\CartItemReader;
+use Spryker\Client\ProductResourceAliasStorage\ProductResourceAliasStorageClientInterface;
 use SprykerShop\Yves\CartPage\CartPageFactory as SprykerShopCartPageFactory;
 
 /**
@@ -22,6 +24,14 @@ class CartPageFactory extends SprykerShopCartPageFactory
     }
 
     /**
+     * @return \FondOfSpryker\Yves\CartPage\Dependency\Client\CartPageToLocaleClientInterface
+     */
+    public function getLocaleClient(): CartPageToLocaleClientInterface
+    {
+        return $this->getProvidedDependency(CartPageDependencyProvider::CLIENT_LOCALE);
+    }
+
+    /**
      * @return \FondOfSpryker\Yves\CartPage\Dependency\Client\CartPageToProductImageStorageClientInterface
      */
     public function getProductImageStorageClient(): CartPageToProductImageStorageClientInterface
@@ -35,5 +45,13 @@ class CartPageFactory extends SprykerShopCartPageFactory
     public function getCartItemTransformerPlugins()
     {
         return $this->getProvidedDependency(CartPageDependencyProvider::PLUGIN_CART_ITEM_TRANSFORMERS);
+    }
+
+    /**
+     * @return \Spryker\Client\ProductResourceAliasStorage\ProductResourceAliasStorageClientInterface
+     */
+    public function getProductAliasStorageClient(): ProductResourceAliasStorageClientInterface
+    {
+        return $this->getProvidedDependency(CartPageDependencyProvider::CLIENT_PRODUCT_ALIAS_STORAGE);
     }
 }
